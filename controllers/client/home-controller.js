afc50b81619ec260acc90a4e6fb46b138e726753
yuetl3mr <yuetl@yuetl>
2024-08-21 14:46:12 +0700

    update homepage

--- controllers/client/home-controller.js
+++ controllers/client/home-controller.js
@@ -1,5 +1,12 @@
-module.exports.index = (req, res) => {
+const Product = require("../../models/product-model");
+
+module.exports.index = async (req, res) => {
+    const Products = await Product.find({
+        status : "active", 
+        deleted : false
+    }).limit(8);
     res.render("./client/pages/home/index", {
-        pageTitle : "Main Page"     
+        pageTitle : "Main Page", 
+        products : Products   
     });
 }
--- models/product-model.js
+++ models/product-model.js
@@ -1,49 +1,49 @@
 const { default: mongoose } = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-      },
-      description: {
-        type: String,
-        required: true,
-      },
-      price: {
-        type: Number,
-        required: true,
-        min: 0,
-      },
-      discountPercentage: {
-        type: Number,
-        required: true,
-        min: 0,
-        max: 100,
-      },
-      stock: {
-        type: Number,
-        required: true,
-        min: 0,
-      },
-      thumbnail: {
-        type: String,
-        required: true,
-      },
-      status: {
-        type: String,
-        enum: ["active", "inactive"],
-        default: "active",
-        required: true,
-      },
-      position: {
-        type: Number,
-        required: true,
-      },
-      deleted: {
-        type: Boolean,
-        default: false,
-        required: true,
-      }
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  discountPercentage: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 100,
+  },
+  stock: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["active", "inactive"],
+    default: "active",
+    required: true,
+  },
+  position: {
+    type: Number,
+    required: true,
+  },
+  deleted: {
+    type: Boolean,
+    default: false,
+    required: true,
+  },
 });
 
 const Product = mongoose.model("Product", productSchema, "products");
